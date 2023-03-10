ccae1fee1f6ed20b116fca5763a39c70d4724f6d
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-10 11:37:30 +0100

    WORK IN PROGESS

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,10 +1,11 @@
 import React, {useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
-import { defaultMethod } from 'react-router-dom/dist/dom';
+
 
 export default function CountryDetails() {
-    const [countryAp]
+    const [countryApiDetails, setcountryApiDetails] = useState(null);
+    const { alpha3Code } = useParams();
 
 
 }
