6c4952ed6a35c2c18136bb307e2593acad1ff1df
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-10 22:55:07 +0100

    Lab Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
@@ -8,10 +8,47 @@ import axios from 'axios';
 
 import countriesData from "./countries.json";
 
+
+
+
 function App() {
+  
+  /*
   const [countries, setCountries] = useState(countriesData);
+  // console.log(countries)
+*/
+  
+  const [countryApi, setcountryApi] = useState([]);
 
+  useEffect(() => {
+    const getCountryData = async () => {
+      try {
+        const data = await axios.get(' https://ih-countries-api.herokuapp.com/countries');
+        setcountryApi(data.data)
+        // console.log(data.data)
+  
+      } catch (error) {
+        console.log(error)
+      }
+    } 
+    getCountryData();
+}, [])
 
-  return <div className="App"></div>;
+ 
+
+  return (
+  <div className="App">
+    <Navbar />
+
+    <div className="container">
+     <CountriesList countries={countryApi}/>
+      <Routes>
+        <Route path="/:alpha3Code" element={<CountryDetails countries={countriesData}  />} />
+      </Routes>
+  </div>
+</div>
+
+  
+  );
 }
 export default App;
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,48 +1,54 @@
-import React, {useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 
-
 export default function CountryDetails() {
-    const [countryApiDetails, setCountryApiDetails] = useState(null);
+   
+
+    const [countryApiDetails, setcountryApiDetails] = useState(null);
     const { alpha3Code } = useParams();
+    console.log(alpha3Code)
 
     useEffect(() => {
         const getCountryData = async () => {
-            try {
-                const data = await axios.get(` https://ih-countries-api.herokuapp.com/countries/${alpha3Code}`);
-                setCountryApiDetails(data.data)
-            }
-            catch (error) {
-                console.log(error)
-            }
-        }
+          try {
+            const data = await axios.get(` https://ih-countries-api.herokuapp.com/countries/${alpha3Code}`);
+            setcountryApiDetails(data.data)
+            // console.log(data.data)
+      
+          } catch (error) {
+            console.log(error)
+          }
+        } 
         getCountryData();
     }, [alpha3Code])
 
-    return (
-        <div className='details'>
-            {countryApiDetails && (
-                <div>
-                    <img src= {`https://flagpedia.net/data/flags/icon/72x54/${countryApiDetails.alpha2Code.toLowerCase()}.png`} alt=""/>
-                    <h2>{countryApiDetails.name.common}</h2>
-                    <p>Capital {countryApiDetails.capital} </p>
-                    <p>Area {countryApiDetails.area} </p>
-                    <div className='borders'>
-                        <p>Borders</p>
-                        <div className='eachBorder'>
-                            {countryApiDetails.borders.map((elem) => {
-                            return (
-                                <div key={elem}>
-                                    <Link to={`/${elem}`}> {elem}</Link>
-                                </div>
-                                ); 
-                            })};
-                        </div>
+  return (
+    <div className='details'>
+    {countryApiDetails && (
+        <div>
+            <img src= {`https://flagpedia.net/data/flags/icon/72x54/${countryApiDetails.alpha2Code.toLowerCase()}.png`} alt=""/>
+            <h2>{countryApiDetails.name.common}</h2>
+            <p>Capital {countryApiDetails.capital} </p>
+            <p>Area {countryApiDetails.area} </p>
+            <div className='borders'>
+            <p>Borders</p>
+            <div className='eachBorder'>
+             {countryApiDetails.borders.map((elem) => {
+                console.log("estooo eee eel eeleeemeeentoooo", elem);
+                return (
+                    <div key={elem}>
+                        <Link to={`/${elem}`}> {elem}</Link>
                     </div>
-                </div>
-            )}; 
-            {!countryApiDetails && <p>Country not found</p>};
+                   
+                ) 
+            })}
+            </div>
+            </div>
         </div>
-    );
-};
+    )} 
+    {!countryApiDetails && <p>Country not found</p>}
+    
+    </div>
+  )
+}
