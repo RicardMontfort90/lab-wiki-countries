720fe3dff82f66703b55de8615b277217f8b90bb
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-10 11:32:53 +0100

    WORK IN PROGESS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import CountriesList from "./components/CountriesList";
+import CountryDetails from "./components/CountryDetails";
+import axios from 'axios';
 
 import countriesData from "./countries.json";
 
---
+++ src/components/CountryDetails.jsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+import { defaultMethod } from 'react-router-dom/dist/dom';
+
+export default function CountryDetails() {
+    const [countryAp]
+
+
+}
