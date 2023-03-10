b485861b1fc07563d9ccbe1958e8b6743b3a0405
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-10 11:14:58 +0100

    WORK IN PROGESS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+import countriesData from "./countries.json";
+
 function App() {
+  const [countries, setCountries] = useState(countriesData);
+
+
   return <div className="App"></div>;
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import "bootstrap/dist/css/bootstrap.css";
+
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,6 +17,5 @@ root.render(
     <App />
   </Router>
 );
-
 
 reportWebVitals();
