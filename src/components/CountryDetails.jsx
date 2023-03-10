69d3124caa023222759083e41c2a415c15158d4b
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-10 23:01:56 +0100

    Lab Done

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -3,52 +3,47 @@ import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default function CountryDetails() {
-   
-
     const [countryApiDetails, setcountryApiDetails] = useState(null);
     const { alpha3Code } = useParams();
-    console.log(alpha3Code)
+    
 
     useEffect(() => {
         const getCountryData = async () => {
-          try {
-            const data = await axios.get(` https://ih-countries-api.herokuapp.com/countries/${alpha3Code}`);
-            setcountryApiDetails(data.data)
-            // console.log(data.data)
-      
-          } catch (error) {
+            try {
+                const data = await axios.get(` https://ih-countries-api.herokuapp.com/countries/${alpha3Code}`);
+                setcountryApiDetails(data.data)
+            } 
+            catch (error) {
             console.log(error)
-          }
+            }
         } 
         getCountryData();
     }, [alpha3Code])
 
-  return (
-    <div className='details'>
-    {countryApiDetails && (
-        <div>
-            <img src= {`https://flagpedia.net/data/flags/icon/72x54/${countryApiDetails.alpha2Code.toLowerCase()}.png`} alt=""/>
-            <h2>{countryApiDetails.name.common}</h2>
-            <p>Capital {countryApiDetails.capital} </p>
-            <p>Area {countryApiDetails.area} </p>
-            <div className='borders'>
-            <p>Borders</p>
-            <div className='eachBorder'>
-             {countryApiDetails.borders.map((elem) => {
-                console.log("estooo eee eel eeleeemeeentoooo", elem);
-                return (
-                    <div key={elem}>
-                        <Link to={`/${elem}`}> {elem}</Link>
+    return (
+        <div className='details'>
+            {countryApiDetails && (
+            <div>
+                <img src= {`https://flagpedia.net/data/flags/icon/72x54/${countryApiDetails.alpha2Code.toLowerCase()}.png`} alt=""/>
+                <h2>{countryApiDetails.name.common}</h2>
+                <p>Capital {countryApiDetails.capital} </p>
+                <p>Area {countryApiDetails.area} </p>
+                <div className='borders'>
+                    <p>Borders</p>
+                    <div className='eachBorder'>
+                        {countryApiDetails.borders.map((elem) => {
+                
+                        return (
+                            <div key={elem}>
+                                <Link to={`/${elem}`}> {elem}</Link>
+                            </div>
+                        ) 
+                })}
                     </div>
-                   
-                ) 
-            })}
+                </div>
             </div>
-            </div>
+            )} 
+                {!countryApiDetails && <p>Country not found</p>}
         </div>
-    )} 
-    {!countryApiDetails && <p>Country not found</p>}
-    
-    </div>
-  )
-}
+    );
+};
